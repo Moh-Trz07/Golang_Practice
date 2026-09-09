7f01c47511afc8a8241d664324f3fdccdd0992a0
Mohammed Trouzine <trzmoh3@gmail.com>
2026-09-09 23:27:08 +0100

    Update main.go

--- Bigger_Nbr/main.go
+++ Bigger_Nbr/main.go
@@ -3,12 +3,22 @@ package main
 import "fmt"
 
 func main() {
-   var numbers = []int{1, 2, 3, 5, 10, 12, 6, 7}
-   num := numbers[0]
-   for _,x := range numbers[1:]{
-    if x > num{
-        num = x
-    } 
-   }
-   fmt.Println(num)
+    var count int
+    fmt.Print("Enter how many numbers: ")
+    fmt.Scan(&count)
+    
+    numbers := make([]int, count)
+    
+    fmt.Printf("Enter %d numbers separated by spaces: ", count)
+    for i := 0; i < count; i++ {
+        fmt.Scan(&numbers[i])
+    }
+    
+    num := numbers[0]
+    for _, x := range numbers[1:] {
+        if x > num {
+            num = x
+        }
+    }
+    fmt.Println("Largest number:", num)
 }
